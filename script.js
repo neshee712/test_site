bef5b865418259c175641e744ba94a0562e9e0cc
neshee712 <nesh7718@gmail.com>
2024-11-13 00:13:15 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yesButton").addEventListener("click", function() {
+    document.getElementById("response").innerText = "YAAAAAAAAAASSSSSSS";
+});
+
+document.getElementById("noButton").addEventListener("click", function() {
+    document.getElementById("response").innerText = "Laa pergila";
+});
